3857d13c5dcfe7ba6377386ba7a313c3e2b7fb20
eddyosos <eduardoquijano2@gmail.com>
2022-07-16 17:39:53 -0300

    command line execution

--- src/main/java/com/github/eddyosos/game_of_life/GameOfLifeApplication.java
+++ src/main/java/com/github/eddyosos/game_of_life/GameOfLifeApplication.java
@@ -1,13 +1,23 @@
 package com.github.eddyosos.game_of_life;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GameOfLifeApplication {
+public class GameOfLifeApplication implements CommandLineRunner {
+
+	private static Logger LOG = LoggerFactory.getLogger(GameOfLifeApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GameOfLifeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		var a = "abc";
+		LOG.info(a);
+	}
 }
